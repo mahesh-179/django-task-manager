47808ad9025d43caa94045c93f7033095be49294
Mahesh-179 <maheshrajlamsal01@gmail.com>
2026-07-22 22:55:17 +0545

    define task model

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Task
 # Register your models here.
+class AdminTask(admin.ModelAdmin):
+    list_display = ['user','task_name','category','assigned_date','priority']
+
+admin.site.register(Task,AdminTask)
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,30 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
+    task_name = models.CharField(max_length=30)
+    task_category = [
+        ('work','Work'),
+        ('personal','Personal'),
+        ('study','Study'),
+        ('shoping','Shoping'),
+        ('health','Health'),
+        ('fitness','Fitness'),
+        ('travel','Travel'),
+        ('other','other'),
+    ]
+    category = models.CharField(max_length=15,choices=task_category)
+    assigned_date = models.DateField(auto_now=True)
+    priority_category = [
+        ('low','Low'),
+        ('medium','Medium'),
+        ('high','High'),
+    ]
+    priority = models.CharField(max_length=10,choices=priority_category)
+    is_completed = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return f"{self.task_name} has been created and is currently in progress {self.priority}"
+    
