20ace0006780dcc65b26721cf1ebf3496064fbe2
Mahesh-179 <maheshrajlamsal01@gmail.com>
2026-07-23 00:51:47 +0545

    configure authenticate

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login,authenticate
+from django.contrib import messages
 # Create your views here.
+def register(request):
+    form = UserCreationForm()
+    if form.is_valid():
+        user = form.save()
+        login(request,user)
+        messages.success(request,"Successfully registered and logged in !!")
+    context ={
+        "form":form,
+    }
+    return render(request,"register.html",context=context)
