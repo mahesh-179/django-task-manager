7a9d3363f123eaf7e7f679448ab6f619188adfaf
Mahesh-179 <maheshrajlamsal01@gmail.com>
2026-07-23 00:08:54 +0545

    work with home html views

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home,name="home"),
+    path('changestatus/<int:id>/',views.change_status,name="change")
 
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Task
 # Create your views here.
 def home(request):
-    return render(request,"home.html")
+    tasks = Task.objects.all().order_by('id')
+    context={
+        "tasks":tasks,
+    }
+    return render(request,"home.html",context=context)
+
+def change_status(request,id):
+    if request.method=="POST":
+        task = Task.objects.get(id=id)
+        task.is_completed = True
+        task.save()
+    return redirect('home')
